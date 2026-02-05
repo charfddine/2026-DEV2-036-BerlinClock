59de412b4f6a3969acc112faf1c732af31c54380
2026-DEV2-036 <2026-DEV2-036@anonymous.local>
2026-02-05 23:08:43 +0100

    test: add BerlinClockConverter and initial seconds tests

---
+++ BerlinClock/BerlinClock/Models/BerlinClockConverter.swift
@@ -0,0 +1,14 @@
+//
+//  BerlinClockConverter.swift
+//  BerlinClock
+//
+//  Created by Me on 05/02/2026.
+//
+
+import Foundation
+
+final class BerlinClockConverter {
+    func convert(time: String) -> [String] {
+     return []
+    }
+}
