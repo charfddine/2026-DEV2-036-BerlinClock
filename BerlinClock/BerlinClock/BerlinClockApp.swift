d398be79d5578b38982b5849b352986575cc1599
2026-DEV2-036 <2026-DEV2-036@anonymous.local>
2026-02-05 22:27:02 +0100

    Initialize Berlin Clock project

---
+++ BerlinClock/BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by Me on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
